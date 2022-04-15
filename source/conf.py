a0d61862afd1bb0b5a94f77a4751987678ef346d
jxLiu-bio <frankliu210@163.com>
2022-04-15 11:26:07 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -55,7 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-	import nbclean, glob
+import nbclean, glob
 
 for filename in glob.glob('**/*.ipynb', recursive=True):
     ntbk = nbclean.NotebookCleaner(filename)
