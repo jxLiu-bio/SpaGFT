3180ca61e6736a5290b9b79cc38357dcf90f3a30
Cankun Wang <flykun0620@gmail.com>
2022-05-17 11:21:55 -0400

    fix path

--- source/conf.py
+++ source/conf.py
@@ -68,4 +68,4 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_show_sphinx = False
 html_logo = '_static/img/SpaGFT_Logo_RGB.png'
-html_style = '_static/style.css'
+html_style = 'style.css'
