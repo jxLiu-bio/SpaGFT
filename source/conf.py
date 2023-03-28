409c6ff63a47c33640b924f3b4e0637469d3b87d
Jixin Liu <80166502+jxLiu-bio@users.noreply.github.com>
2023-03-28 18:35:41 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -67,5 +67,5 @@ html_theme = 'sphinx_rtd_theme'
 # )
 html_static_path = ['_static']
 html_show_sphinx = False
-html_logo = '_static/img/SpaGFT LOGO.png'
+html_logo = '_static/img/SpaGFT_LOGO.png'
 html_style = 'css/style.css'
